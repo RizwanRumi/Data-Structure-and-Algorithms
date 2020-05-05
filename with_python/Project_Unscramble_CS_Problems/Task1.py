6f6d28b9ea6f7bdc603682565c4a4be30d8d8667
rizwanrumi <rizwan.cse33@gmail.com>
2020-05-05 15:55:36 +0200

    Add Task1.py file

---
+++ with_python/Project_Unscramble_CS_Problems/Task1.py
@@ -0,0 +1,34 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+
+telephone_list = []
+
+for text_number in texts:
+    telephone_list.append(text_number[0])
+    telephone_list.append(text_number[1])
+
+for call_number in calls:
+    telephone_list.append(call_number[0])
+    telephone_list.append(call_number[1])
+
+unique_numbers = set(telephone_list)
+
+print("There are {0} different telephone numbers in the records.".format(len(unique_numbers)))
