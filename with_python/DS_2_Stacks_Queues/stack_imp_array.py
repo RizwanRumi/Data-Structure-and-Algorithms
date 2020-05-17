e2fd9a5214b4b198f61171ce3849f4a995a7d4c8
rizwanrumi <rizwan.cse33@gmail.com>
2020-05-18 01:27:17 +0200

    add stack implementation using array

---
+++ with_python/DS_2_Stacks_Queues/stack_imp_array.py
@@ -0,0 +1,59 @@
+class Stack:
+
+    def __init__(self, initial_size=10):
+        self.arr = [0 for _ in range(initial_size)]
+        self.next_index = 0
+        self.num_elements = 0
+
+    def push(self, data):
+        if self.next_index == len(self.arr):
+            print("Out of space! Increasing array capacity ...")
+            self._handle_stack_capacity_full()
+
+        self.arr[self.next_index] = data
+        self.next_index += 1
+        self.num_elements += 1
+
+    def pop(self):
+        if self.is_empty():
+            self.next_index = 0
+            return None
+        self.next_index -= 1
+        self.num_elements -= 1
+        return self.arr[self.next_index]
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.num_elements == 0
+
+    def _handle_stack_capacity_full(self):
+        old_arr = self.arr
+
+        self.arr = [0 for _ in range(2 * len(old_arr))]
+        for index, element in enumerate(old_arr):
+            self.arr[index] = element
+
+
+foo = Stack()
+print(foo.arr)
+foo.push("Test!")
+foo.push(42)
+foo.push("fish")
+foo.push(1)
+foo.push(2)
+foo.push(3)
+foo.push(4)
+foo.push(5)
+foo.push(6)
+foo.push(7)
+foo.push(8)
+foo.push(9)
+foo.push(10)
+foo.push(11)
+print(foo.arr)
+
+print('---pop from stack---')
+print(foo.pop())
+print(foo.pop())
