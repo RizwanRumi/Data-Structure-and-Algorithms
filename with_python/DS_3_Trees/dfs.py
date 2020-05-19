c5098b058a72e45cac3cbb81332ba968e47bb5ff
rizwanrumi <rizwan.cse33@gmail.com>
2020-05-20 01:00:13 +0200

    modified pre order traversing with tracking

--- with_python/DS_3_Trees/dfs.py
+++ with_python/DS_3_Trees/dfs.py
@@ -112,6 +112,7 @@ print("\n")
 print(stack)
 '''
 
+'''
 visit_order = list()
 stack = Stack()
 
@@ -300,4 +301,91 @@ dfs_visited = pre_order_with_stack_buggy(tree)
 for val in dfs_visited:
     print(val)
 
+"""
+pre-order traversal using a stack, tracking state
+Here's how we implement DFS with a stack, where we also track whether 
+we've already visited the left or right child of the node.
+"""
+print("----------tracking state----------")
+'''
 
+class State(object):
+    def __init__(self, node):
+        self.node = node
+        self.visited_left = False
+        self.visited_right = False
+
+    def get_node(self):
+        return self.node
+
+    def get_visited_left(self):
+        return self.visited_left
+
+    def get_visited_right(self):
+        return self.visited_right
+
+    def set_visited_left(self):
+        self.visited_left = True
+
+    def set_visited_right(self):
+        self.visited_right = True
+
+    def __repr__(self):
+        s = f"""{self.node}
+visited_left: {self.visited_left}
+visited_right: {self.visited_right}
+        """
+        return s
+
+def pre_order_with_stack(tree, debug_mode=False):
+    visit_order = list()
+    stack = Stack()
+    node = tree.get_root()
+    visit_order.append(node.get_value())
+    state = State(node)
+    stack.push(state)
+    count = 0
+    while (node):
+        if debug_mode:
+            print(f"""
+loop count: {count}
+current node: {node}
+stack:
+{stack}
+            """)
+        count += 1
+        if node.has_left_child() and not state.get_visited_left():
+            state.set_visited_left()
+            node = node.get_left_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+            stack.push(state)
+
+        elif node.has_right_child() and not state.get_visited_right():
+            state.set_visited_right()
+            node = node.get_right_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+
+        else:
+            stack.pop()
+            if not stack.is_empty():
+                state = stack.top()
+                node = state.get_node()
+            else:
+                node = None
+
+    if debug_mode:
+        print(f"""
+loop count: {count}
+current node: {node}
+stack:
+{stack}
+            """)
+    return visit_order
+
+# check pre-order traversal
+pre_order_result = pre_order_with_stack(tree, debug_mode=True)
+
+for val in pre_order_result:
+    print(val)
