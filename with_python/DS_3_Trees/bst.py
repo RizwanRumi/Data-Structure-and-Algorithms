74d3ad0c1100e900a19fcb6db76f1422f9a8b8af
rizwanrumi <rizwan.cse33@gmail.com>
2020-05-20 17:25:11 +0200

    add binary search tree

---
+++ with_python/DS_3_Trees/bst.py
@@ -0,0 +1,77 @@
+"""
+Binary Search Tree
+
+# Nodes have an order
+# insert : log(n)
+# search : log(n)
+# delete : log(n)
+
+Use : Queue
+
+"""
+
+from collections import deque
+
+class Node(object):
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left != None
+
+    def has_right_child(self):
+        return self.right != None
+
+    # define __repr_ to decide what a print statement displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+class Queue:
+    def __init__(self):
+        self.q = deque()
+
+    def enq(self, value):
+        self.q.appendleft(value)
+
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.q)
+
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
